fb7c5d697fcf04b91c13af81457a64435f8967da
Adrian <adrian.george.radu@gmail.com>
2023-09-26 13:52:22 +0200

    Apps and models for simulation and dependencies created

--- backend/simulations/models.py
+++ backend/simulations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users import models as users
+from applications import models as applications
+from dependencies import models as dependencies
 
 # Create your models here.
+
+class Simulations(models.Model):
+    user_id = models.ForeignKey(users.Users, on_delete=models.CASCADE)
+    application_id = models.ForeignKey(applications.Applications, on_delete=models.CASCADE)
+    recruiter_attitude = models.CharField(max_length=150, null=True, blank=True)
+    language_id = models.ForeignKey(dependencies.Languages, on_delete=models.CASCADE)
+    json_file = models.TextField(null=True, blank=True)
+    review = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField()
