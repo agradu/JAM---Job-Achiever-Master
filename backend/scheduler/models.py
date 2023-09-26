703161d5c8caa21f90c2bda0a8bad647fb529f38
semen_zamai <zamay.sv@gmail.com>
2023-09-26 14:31:12 +0200

    add scheduler app with views, models, serialazers, urls

---
+++ backend/scheduler/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.first_name
+    
+class Company(models.Model):
+    company_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.company_name
