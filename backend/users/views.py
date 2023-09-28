74e098986cbcffdcbb5b2061a8b64e9ca9b5a18b
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-09-28 11:50:45 +0200

    Remove redundant views from user app

--- backend/users/views.py
+++ backend/users/views.py
@@ -9,15 +9,9 @@ from .serializers import (
     UserHobbySerializer,
 )
 from .models import User, Education, Experience, UserSkill, UserLanguage, UserHobby
-from rest_framework import generics
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -27,17 +21,7 @@ class EducationViewSet(viewsets.ModelViewSet):
     serializer_class = EducationSerializer
 
 
-class EducationDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Education.objects.all()
-    serializer_class = EducationSerializer
-
-
 class ExperienceViewSet(viewsets.ModelViewSet):
-    queryset = Experience.objects.all()
-    serializer_class = ExperienceSerializer
-
-
-class ExperienceDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
 
@@ -47,26 +31,11 @@ class UserSkillViewSet(viewsets.ModelViewSet):
     serializer_class = UserSkillSerializer
 
 
-class UserSkillDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = UserSkill.objects.all()
-    serializer_class = UserSkillSerializer
-
-
 class UserLanguageViewSet(viewsets.ModelViewSet):
     queryset = UserLanguage.objects.all()
     serializer_class = UserLanguageSerializer
 
 
-class UserLanguageDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = UserLanguage.objects.all()
-    serializer_class = UserLanguageSerializer
-
-
 class UserHobbyViewSet(viewsets.ModelViewSet):
-    queryset = UserHobby.objects.all()
-    serializer_class = UserHobbySerializer
-
-
-class UserHobbyDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserHobby.objects.all()
     serializer_class = UserHobbySerializer
