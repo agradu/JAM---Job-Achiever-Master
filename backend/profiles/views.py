fd370851f91fa197ca197b997848b8f49db971a0
Adrian <adrian.george.radu@gmail.com>
2023-10-06 08:49:01 +0200

    Trying to understend how to create/update

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, status
 from .serializers import (
     ProfileSerializer,
     EducationSerializer,
@@ -9,6 +8,7 @@ from .serializers import (
     ProfileHobbySerializer,
 )
 from .models import Profile, Education, Experience, ProfileSkill, ProfileLanguage, ProfileHobby
+from rest_framework.response import Response
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -21,11 +21,25 @@ class EducationViewSet(viewsets.ModelViewSet):
     serializer_class = EducationSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ExperienceViewSet(viewsets.ModelViewSet):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProfileSkillViewSet(viewsets.ModelViewSet):
@@ -33,14 +47,35 @@ class ProfileSkillViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSkillSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProfileLanguageViewSet(viewsets.ModelViewSet):
     queryset = ProfileLanguage.objects.all()
     serializer_class = ProfileLanguageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProfileHobbyViewSet(viewsets.ModelViewSet):
     queryset = ProfileHobby.objects.all()
     serializer_class = ProfileHobbySerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
