64d90321c69f625cb57242006285599c05f4ef08
Adrian <adrian.george.radu@gmail.com>
2023-10-11 14:08:51 +0200

    Fixed dependencies models.py - all fields unique.

--- backend/dependencies/models.py
+++ backend/dependencies/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 
 class Language(models.Model):
-    language = models.CharField(max_length=100, default="English")
+    language = models.CharField(max_length=100, default="English", unique=True)
     def __str__(self):
         return self.language 
 
 class Gender(models.Model):
-    gender = models.CharField(max_length=50, default="Male")
+    gender = models.CharField(max_length=50, default="Male", unique=True)
     def __str__(self):
         return self.gender 
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=100, default="Saved")
+    status = models.CharField(max_length=100, default="Saved", unique=True)
     def __str__(self):
         return self.status 
