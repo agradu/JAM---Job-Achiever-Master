dd6f1f43279bb4996be4ae8eb8fd23375b904ac5
miclovich <vicmiclovich@gmail.com>
2023-10-13 17:11:27 +0200

    Cleanup tests

--- backend/accounts/tests/test_setup.py
+++ backend/accounts/tests/test_setup.py
@@ -3,20 +3,13 @@ from django.urls import reverse
 
 
 class TestSetUp(APITestCase):
-    
     def setUp(self):
         self.register_url = reverse("user-registration")
-        #self.login_url = reverse("user-login")
-        
-        self.user_data={
-            "email": "fake_user@example.com",
-            "username":"fake_user",
-            "first_name":"John",
-            "last_name":"Doe",
-            "password": "A v3ry d1fficult p@$$w0rD!"
-        }
-        
-        return super().setUp()
 
-    def tearDpwn(self):
-        return super().tearDown()
+        self.user_data = {
+            "email": "fake_user@example.com",
+            "username": "fake_user",
+            "first_name": "John",
+            "last_name": "Doe",
+            "password": "A v3ry d1fficult p@$$w0rD!",
+        }
--- backend/accounts/tests/test_views.py
+++ backend/accounts/tests/test_views.py
@@ -1,41 +1,35 @@
-from django.test import TestCase
 from .test_setup import TestSetUp
+from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your tests here.
 
 class TestViews(TestSetUp):
     def test_user_cannot_register_with_no_data(self):
-        res =  self.client.post(self.register_url)
+        res = self.client.post(self.register_url)
         self.assertEqual(res.status_code, 400)
-    
+
     def test_user_can_register_correctly(self):
-        res = self.client.post(
-            self.register_url, self.user_data, format="json")
-        self.assertEqual(res.data['user_id'], 1)
+        res = self.client.post(self.register_url, self.user_data, format="json")
+        self.assertEqual(res.data["user_id"], 1)
         self.assertEqual(res.status_code, 201)
-        
-    # def test_user_cannot_login_with_unverified_email(self):
-    #     res =  self.client.post(
-    #         self.register_url, self.user_data, format="json")
-    #     breakpoint()
-    #     res = self.client.post(self.login_url, self.user_data, format="json")
-    #     self.assertEqual(res.status_code, 401)
-    
-    # def test_user_cannot_login_after_verification(self):
-    #     response = self.client.post(
-    #         self.register_url, self.user_data, format="json")
-    #     self.assertEqual(response.status_code, 201)  # Check for a successful registration
 
-    #     user_id = response.data.get('user_id')
-    #     self.assertIsNotNone(user_id)
+    def test_user_login(self):
+        user = User(**self.user_data)
+        user.set_password(user.password)
+        user.save()
+        res = self.client.post(reverse("rest_login"), self.user_data, format="json")
+        self.assertEqual(res.status_code, 200)
+        # check for Token (also known as key)
+        self.assertTrue("key" in res.json())
 
-    #     # Retrieve the user by user_id and update the is_verified flag
-    #     user = User.objects.get(id=user_id)
-    #     user.is_verified = True
-    #     user.save()
-    #     user.refresh_from_db()  # Refresh the user object from the database
-
-    #     # Attempt to log in
-    #     login_response = self.client.post(self.login_url, self.user_data, format="json")
-    #     self.assertEqual(login_response.status_code, 200)  # Check for successful login after verification
+    def test_user_login_fail(self):
+        res = self.client.post(
+            reverse("rest_login"),
+            {"username": "doesnotexist", "password": "password"},
+            format="json",
+        )
+        self.assertEqual(res.status_code, 400)
+        self.assertDictEqual(
+            res.json(),
+            {"non_field_errors": ["Unable to log in with provided credentials."]},
+        )
