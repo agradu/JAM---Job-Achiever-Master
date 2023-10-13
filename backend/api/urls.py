a916a7ec9c9778d147f9dab4e7f2f4e37e09f20b
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-10-13 11:21:34 +0200

    Add the swagger file

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView,  SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,4 +30,6 @@ urlpatterns = [
     path("api/v1/schedulers/", include("schedulers.urls")),
     path("api/v1/simulations/", include("simulations.urls")),
     path("api/v1/email_sender/", include("email_sender.urls")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
