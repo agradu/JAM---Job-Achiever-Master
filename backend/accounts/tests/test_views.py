a61c7dec168fd0db27bd7140537dd77490b4b7b4
miclovich <vicmiclovich@gmail.com>
2023-10-13 22:18:52 +0200

    put everything in one file

--- backend/accounts/tests/test_views.py
+++ backend/accounts/tests/test_views.py
@@ -1,9 +1,21 @@
-from .test_setup import TestSetUp
+from rest_framework.test import APITestCase
+from django.urls import reverse
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class TestViews(TestSetUp):
+class TestViews(APITestCase):
+    def setUp(self):
+        self.register_url = reverse("user-registration")
+
+        self.user_data = {
+            "email": "fake_user@example.com",
+            "username": "fake_user",
+            "first_name": "John",
+            "last_name": "Doe",
+            "password": "A v3ry d1fficult p@$$w0rD!",
+        }
+
     def test_user_cannot_register_with_no_data(self):
         res = self.client.post(self.register_url)
         self.assertEqual(res.status_code, 400)
