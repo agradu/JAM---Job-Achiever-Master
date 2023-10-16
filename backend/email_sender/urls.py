fec66b28e950240af0a271fbdcf001e1788c30fa
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-10-16 22:31:55 +0200

    Create the folders/file for API tests, add the attachment function to email_sender.views, clean up email_sender.urls

--- backend/email_sender/urls.py
+++ backend/email_sender/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import Sendmail
 
 urlpatterns = [path("mail/", Sendmail.as_view(), name="sendmail")]
--- backend/email_sender/views.py
+++ backend/email_sender/views.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from rest_framework import permissions
 from rest_framework.views import APIView
-from django.core import mail
-from rest_framework.response import Response
+from rest_framework import permissions
+from django.core.mail import EmailMessage
 from applications.models import Application
-
+from rest_framework.response import Response
+from generator_cv.views import UpdateCvDescriptionWithGPT
 
 # Create your views here.
 class Sendmail(APIView):
@@ -13,12 +13,15 @@ class Sendmail(APIView):
     def post(self, request):
         try:
             application = Application.objects.get(pk=request.data["pk"])
-            email = mail.EmailMessage(
+            email = EmailMessage(
                 subject=f"Application for: {application.position}",
                 body=application.cover_letter_text,
                 from_email=settings.EMAIL_HOST_USER,
                 to=[application.company_email],
             )
+            
+            email.attach("cv")
+            
             email.send(fail_silently=False)
             return Response({"status": True, "message": "Email sent successfully"})
         except:
