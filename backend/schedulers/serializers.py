e6f06f76184d3692aff0c87514c3fc9068c8baa3
miclovich <vicmiclovich@gmail.com>
2023-10-18 17:34:57 +0200

    Refactor code - schedulers tests
    Co-authored-by: Semen Zamai <s.zamai@weplay.tv>

--- backend/schedulers/serializers.py
+++ backend/schedulers/serializers.py
@@ -2,7 +2,7 @@ from .models import Scheduler
 from rest_framework import serializers
 
 
-class SchedulerSerialazer(serializers.ModelSerializer):
+class SchedulerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scheduler
         fields = "__all__"
--- backend/schedulers/tests.py
+++ backend/schedulers/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
 from .models import Scheduler
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from applications.models import Application
 from profiles.models import Profile
 from dependencies.models import Gender, Language, Status
@@ -10,38 +11,72 @@ from django.urls import reverse
 
 
 # Create your tests here.
-class SchedulerTest(TestCase):
-    def test_scheduler_creation(self):
-        user = User.objects.create(username="test", password="123")
-        user.save()
-        gender = Gender.objects.create(gender="Male")
-        gender.save()
-        language = Language.objects.create(language="English")
-        language.save()
-        status = Status.objects.create(status="Saved")
-        status.save()
-        profile = Profile.objects.create(user=user, gender=gender)
-        profile.save()
-        application = Application.objects.create(
-            profile=profile,
+class SchedulerTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.schedule_route = reverse("scheduler-list")
+
+        self.user = User.objects.create(username="test", password="123")
+        self.gender = Gender.objects.create(gender="Male")
+        self.language = Language.objects.create(language="English")
+        self.status = Status.objects.create(status="Saved")
+        self.profile = Profile.objects.create(user=self.user, gender=self.gender)
+        self.application = Application.objects.create(
+            profile=self.profile,
             position="front",
             company="test",
             description="test",
             company_email="test@test.com",
-            application_language=language,
-            status=status,
-            recruiter_gender=gender,
+            application_language=self.language,
+            status=self.status,
+            recruiter_gender=self.gender,
         )
-        application.save()
-        time_saved = datetime.datetime.now()
+
+        self.scheduler_data = {
+            "interview_shedule": "2023-10-18T15:29:43.477Z",
+            "interview_time_before": "14:00:00",
+            "interview_time_after": "16:00:00",
+            "user": self.user.pk,
+            "application": self.application.pk,
+        }
+
+        self.token = Token.objects.create(user=self.user)
+        self.token_header = f"Token {self.token.key}"
+
+    def test_scheduler_cannot_create_with_no_data(self):
+        self.client.credentials(HTTP_AUTHORIZATION=self.token_header)
+        res = self.client.post(self.schedule_route)
+        self.assertEqual(res.status_code, 400)
+
+    def test_scheduler_post(self):
+        self.client.credentials(HTTP_AUTHORIZATION=self.token_header)
+        res = self.client.post(self.schedule_route, self.scheduler_data, format="json")
+
+        self.assertEqual(res.status_code, 201)
+        # always confirm with some real data
+        copy_of_response = res.data.copy()
+        del copy_of_response["id"]
+        self.assertDictEqual(
+            copy_of_response,
+            {
+                "interview_shedule": "2023-10-18T17:29:43.477000+02:00",
+                "interview_time_before": "14:00:00",
+                "interview_time_after": "16:00:00",
+                "user": self.user.pk,
+                "application": self.application.pk,
+            },
+        )
+
+    def test_scheduler_creation(self):
+        # unit test
+        time_saved = timezone.now()
         time_before = time_saved - datetime.timedelta(hours=1)
         time_after = time_saved + datetime.timedelta(hours=1)
         scheduler = Scheduler.objects.create(
-            user=user,
-            application=application,
+            user=self.user,
+            application=self.application,
             interview_shedule=time_saved,
             interview_time_before=time_before,
             interview_time_after=time_after,
         )
-        scheduler.save()
         self.assertEqual(time_saved, scheduler.interview_shedule)
