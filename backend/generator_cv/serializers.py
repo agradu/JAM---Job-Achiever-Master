94e660bd1cfdd8941dc1f6480dafdb4945d42f18
miclovich <vicmiclovich@gmail.com>
2023-10-18 14:14:06 +0200

    Clean up code Run PEP8 with black rules Remove unused files

--- backend/generator_cv/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from applications.models import Application
-
-
-class CVSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Application
-        fields = ["cv_short_description"]
--- backend/generator_cv/views.py
+++ backend/generator_cv/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework import permissions
 from rest_framework.response import Response
 from profiles.models import (
     Education,
